f94d3f2cca83f4a323d1afb711e7cf7a6086c8db
sultan <sultan@xx.com>
2021-05-12 20:31:51 +0000

    best practice modifications

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("By working via pull requests, we can more easily work within our team.")
-print("A pull request is a moment to review, before we use a merge strategy to add our changes.")
+#!/usr/bin/python3
+
+def main():
+    """called at runtime"""
+    
+    # display to the screen
+    print("By working via pull requests, we can more easily work within our team.")
+    print("A pull request is a moment to review, before we use a merge strategy to add our changes.")
+
+if __name__ == "__main__":
+    main()
 
